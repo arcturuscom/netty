4ebddcba8059de165beb36bb1badebfd16d74eaa
Arc <benyazidanass@gmail.com>
2022-04-17 01:16:37 +0000

    Initial commit

---
+++ src/main/java/io/netty/native/epoll/Application.java
@@ -0,0 +1,10 @@
+package io.netty.native.epoll;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
